275efbaa23e80bb9530026891eda50c7b06bc9ff
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-12-21 19:34:49 +0530

    nave bar and footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
