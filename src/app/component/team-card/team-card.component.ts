2fe530041b578e62592cf16e71bb770c45e73dac
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-12-22 00:42:50 +0530

    card

--- src/app/component/team-card/team-card.component.ts
+++ src/app/component/team-card/team-card.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './team-card.component.css'
 })
 export class TeamCardComponent {
-
+  name = "Rajveer choudahary";
+  Profession = "PUBG Player";
+  path = "../../../assets/photos/team member.jpg";
+  LinkedInlink = "#";
+  GitHublink = "#";
 }
