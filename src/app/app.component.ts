8cfafc378ce6b1a7ade64be0ce7332573200beef
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-12-21 20:32:46 +0530

    comonents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { TeamCardComponent } from './component/team-card/team-card.component';
+import { TeamComponent } from './component/team/team.component';
+import { AboutusComponent } from './component/aboutus/aboutus.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavBarComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, TeamCardComponent, TeamComponent, AboutusComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
