b5d928693b33152546219fc81462d2948edfdd1e
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-12-23 22:19:15 +0530

    m

--- src/app/component/team/team.component.ts
+++ src/app/component/team/team.component.ts
@@ -4,7 +4,7 @@ import { TeamCardComponent } from '../team-card/team-card.component';
 export interface TeamMember {
   name: string;
   path: string;
-  Profession: string;
+  Designation: string;
   LinkedInlink: string;
   GitHublink: string;
 }
@@ -16,26 +16,96 @@ export interface TeamMember {
   styleUrl: './team.component.css',
 })
 export class TeamComponent {
-  getTeamMembers(): TeamMember[] {
+  getMentor(): TeamMember[] {
     return [
       {
-        name: 'John Doe',
+        name: 'mentor',
         path: './../../../assets/photos/team member.jpg',
-        Profession: 'Developer',
+        Designation: 'Developer',
         LinkedInlink: 'https://www.linkedin.com/in/johndoe/',
         GitHublink: 'https://github.com/johndoe',
       },
     ];
   }
-  getmentors(): TeamMember[] {
+  getCurrentScholars(): TeamMember[] {
     return [
       {
-        name: 'Dhritideepa sarnia',
+        name: 'CurrentScholars',
         path: './../../../assets/photos/team member.jpg',
-        Profession: 'Dancer',
+        Designation: 'Dancer',
         LinkedInlink: 'https://www.linkedin.com/in/johndoe/',
         GitHublink: 'https://github.com/johndoe',
       },
     ];
+  }
+  getCurrentmembers(): TeamMember[] {
+    return [
+      {
+        name: 'Currentmembers',
+        path: './../../../assets/photos/team member.jpg',
+        Designation: 'Dancer',
+        LinkedInlink: 'https://www.linkedin.com/in/johndoe/',
+        GitHublink: 'https://github.com/johndoe',
+      },
+    ];
+  }
+  getfoundingmembers(): TeamMember[] {
+    return [
+      {
+        name: 'foundingmembers',
+        path: './../../../assets/photos/team member.jpg',
+        Designation: 'Dancer',
+        LinkedInlink: 'https://www.linkedin.com/in/johndoe/',
+        GitHublink: 'https://github.com/johndoe',
+      },
+    ];
+  }
+  getpastmembers(): TeamMember[] {
+    return [
+      {
+        name: 'pastmembers',
+        path: './../../../assets/photos/team member.jpg',
+        Designation: 'Dancer',
+        LinkedInlink: 'https://www.linkedin.com/in/johndoe/',
+        GitHublink: 'https://github.com/johndoe',
+      },
+    ];
+  }
+  removeclass(): void {
+    let css = document.getElementById("Mentor");
+    css?.classList.add('deactiveclass');
+    let css1 = document.getElementById("CurrentScholars");
+    css1?.classList.remove('activeclass');
+    let css3 = document.getElementById("Currentmembers");
+    css3?.classList.remove('activeclass');
+    let css4 = document.getElementById("foundingmembers");
+    css4?.classList.remove('activeclass');
+    let css5 = document.getElementById("pastmembers");
+    css5?.classList.remove('activeclass');
+  }
+  Mentor(): void {
+    console.log("mentor");
+    document.getElementById("Mentor")?.classList.add('activeclass');
+    this.removeclass();
+  }
+  CurrentScholars(): void {
+    console.log("currentscholers");
+    this.removeclass();
+    document.getElementById("CurrentScholars")?.classList.add('activeclass');
+  }
+  Currentmembers(): void {
+    console.log("currentmembers");
+    this.removeclass();
+    let css3 = document.getElementById("Currentmembers")?.classList.add('activeclass');
+  }
+  foundingmembers(): void {
+    console.log("founding members");
+    this.removeclass();
+    let css4 = document.getElementById("foundingmembers")?.classList.add('activeclass');
+  }
+  pastmembers(): void {
+    console.log("past members");
+    this.removeclass();
+    let css5 = document.getElementById("pastmembers")?.classList.add('activeclass');
   }
 }
