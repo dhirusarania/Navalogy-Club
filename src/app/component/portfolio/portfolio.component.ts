caea26f44a272cdcc6649db4d5d14270a9d0ae99
vivekrai9900 <raivivek9900@gmail.com>
2023-12-25 00:50:14 +0530

    portfolio started

--- src/app/component/portfolio/portfolio.component.ts
+++ src/app/component/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
