c3805baa5cb627513420a8637101a87cde619ae6
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2024-01-03 02:09:49 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Page404Component } from './page-404/page-404.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: '**', component: Page404Component },
+    { path: '/', component:AppComponent },
+    
 ];
