51471ce3c285b458041a536cbeecb480822c1ada
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2024-01-03 02:11:16 +0530

    b

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Page404Component } from './page-404/page-404.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
     { path: '**', component: Page404Component },
-    { path: '/', component:AppComponent },
+    { path: './', component:HomeComponent },
     
 ];
