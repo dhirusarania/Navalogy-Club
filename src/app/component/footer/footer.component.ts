0f33219bee9187cb9d89d5fe648d1b569f2f4733
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2024-01-03 01:14:24 +0530

    done

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -9,5 +9,22 @@ import { Page404Component } from '../../page-404/page-404.component';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  homepage(): void {
+    document.getElementById("homepage")?.scrollIntoView({ behavior: "smooth" });
+  }
+  aboutpage(): void {
+    document.getElementById("aboutuspage")?.scrollIntoView({ behavior: "smooth" });
+  }
+  publicationspage(): void {
+    document.getElementById("publicationspage")?.scrollIntoView({ behavior: "smooth" });
+  }
+  portfoliopage(): void {
+    document.getElementById("portfoliopage")?.scrollIntoView({ behavior: "smooth" });
+  }
+  teampage(): void {
+    document.getElementById("teampage")?.scrollIntoView({ behavior: "smooth" });
+  }
+  contactuspage(): void {
+    document.getElementById("contactuspage")?.scrollIntoView({ behavior: "smooth" });
+  }
 }
